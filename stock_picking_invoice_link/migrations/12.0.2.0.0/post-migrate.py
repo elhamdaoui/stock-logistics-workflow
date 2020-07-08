926ab56da38168392cc01349fe95926b274c3345
Elhamdaoui Abdelmajid <elhamdaouiabdelmajid@gmail.com>
2020-07-08 21:53:11 +0100

    21:40:01	ERROR	server	module stock_picking_invoice_link: Each post-migration file must have a "migrate(cr, installed_version)" function

--- stock_picking_invoice_link/migrations/12.0.2.0.0/post-migrate.py
+++ stock_picking_invoice_link/migrations/12.0.2.0.0/post-migrate.py
@@ -4,13 +4,13 @@ from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
-def migrate(env, version):
+def migrate(cr, version):
     # Convert invoice_line_id Many2one field to Many2many field if relation
     # table is empty.
     # This is due to a commit from v11 done after v12 migration, so is possible
     # that this field is already converted
     sql = "SELECT COUNT(*) FROM  stock_move_invoice_line_rel;"""
-    env.cr.execute(sql)
-    if not env.cr.fetchone():
-        openupgrade.m2o_to_x2m(env.cr, env['stock.move'], 'stock_move',
+    cr.execute(sql)
+    if not cr.fetchone():
+        openupgrade.m2o_to_x2m(cr, env['stock.move'], 'stock_move',
                                'invoice_line_ids', 'invoice_line_id')
